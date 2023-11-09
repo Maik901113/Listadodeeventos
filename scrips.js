0fe72968fc892309f6416c6d90a0408221941280
michael-garcia <mdgarcia6203@misena.edu.co>
2023-11-08 23:48:06 -0500

    listado de eventos

---
+++ scrips.js
@@ -0,0 +1,93 @@
+let eventos = [];
+let arr = [];
+
+const nombreEvento=document.querySelector("#nombreEvento");
+const fechaEvento=document.querySelector("#fechaEvento");
+const botnAgregar=document.querySelector("#agregar");
+const listaEventos=document.querySelector("#listaEventos");
+
+
+const json = carga() ;
+try{
+    arr = JSON.parse(json);
+} catch (error){
+    arr=[];
+}
+eventos = arr? [...arr] : [];
+mostrarEventos();
+
+
+
+document.querySelector("form").addEventListener("submit", e =>{
+    e.preventDefault();
+    agregarEvento();
+
+
+});
+function agregarEvento(){
+    if(nombreEvento.value ===""|| fechaEvento.value===""){
+        return;
+    }
+    if( diferenciasFecha (fechaEvento.value) < 0){
+        return;
+    }
+    const nuevoEvento = {
+        id: (Math.random()*100).toString(36).slice(3),
+        nombre: nombreEvento.value,
+        fecha: fechaEvento.value,
+    };
+    eventos.unshift(nuevoEvento);
+
+    guardar(JSON.stringify(eventos));
+
+    nombreEvento.value="";
+
+    mostrarEventos();
+
+}
+function diferenciasFecha(destino){
+    let fechaDestino = new Date(destino);
+    let fechaActal = new Date();
+    let diferencias =fechaDestino.getTime()-fechaActal.getTime();
+    let dias= Math.ceil(diferencias/(1000*3600*24));
+    return dias;
+}
+
+function mostrarEventos(){
+    const eventoshtml=eventos.map((evento) =>{
+        return `
+        <div class="evento">
+         <div class="dias"> 
+           <span class="diasFaltantes">${diferenciasFecha(evento.fecha)}</span>
+          <span class="texto"> dias para</span>
+         </div>
+         
+         
+         <div class ="nombreEvento">${evento.nombre}</div>
+         <div class ="fechaEvento">${evento.fecha}</div>
+         <div class="acciones">
+          <button data-id="${evento.id}"class="eliminar">Eliminar</button>
+        </div>            
+        
+        `;
+
+    })
+    listaEventos.innerHTML=eventoshtml.join("");
+    document.querySelectorAll('.eliminar').forEach(button=>{
+        button.addEventListener("click", e =>{
+            const id=button.getAttribute('data-id')
+            eventos=eventos.filter(evento=>evento.id !==id);
+            guardar(JSON.stringify(eventos));
+
+            mostrarEventos();
+        });
+
+
+    });
+}
+function guardar(datos){
+    localStorage.setItem("lista", datos);
+}
+function carga(){
+    return localStorage.getItem("lista");
+}
